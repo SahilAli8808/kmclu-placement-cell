84df336a0789e1b0641b44d3ab6fd2c27360d2aa
Subhan Raj <shubhanraj2002@gmail.com>
2022-11-09 11:32:00 +0530

    fixed pwa
    Co-Authored-By: SAHIL ALI <84962462+SahilAli8808@users.noreply.github.com>
    Co-Authored-By: Vaishnavi Mishra <113234737+mishravaishnavi@users.noreply.github.com>

--- assets/js/service-worker.js
+++ assets/js/service-worker.js
@@ -1,51 +1,60 @@
+importScripts(
+    'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
+);
 
-  
-    const HOSTNAME_WHITELIST = [
-        self.location.hostname,
-        'fonts.gstatic.com',
-        'fonts.googleapis.com',
-        'cdn.jsdelivr.net'
-    ]
+workbox.routing.registerRoute(
+    ({
+        Request
+    }) => Request.destination === 'image',
+    new workbox.strategies.NetworkFirst()
+);
 
-    // The Util Function to hack URLs of intercepted requests
-    const getFixedUrl = (req) => {
-        var now = Date.now()
-        var url = new URL(req.url)
+const HOSTNAME_WHITELIST = [
+    self.location.hostname,
+    'fonts.gstatic.com',
+    'fonts.googleapis.com',
+    'cdn.jsdelivr.net'
+]
 
-        // 1. fixed http URL
-        // Just keep syncing with location.protocol
-        // fetch(httpURL) belongs to active mixed content.
-        // And fetch(httpRequest) is not supported yet.
-        url.protocol = self.location.protocol
+// The Util Function to hack URLs of intercepted requests
+const getFixedUrl = (req) => {
+    var now = Date.now()
+    var url = new URL(req.url)
 
-        // 2. add query for caching-busting.
-        // Github Pages served with Cache-Control: max-age=600
-        // max-age on mutable content is error-prone, with SW life of bugs can even extend.
-        // Until cache mode of Fetch API landed, we have to workaround cache-busting with query string.
-        // Cache-Control-Bug: https://bugs.chromium.org/p/chromium/issues/detail?id=453190
-        if (url.hostname === self.location.hostname) {
-            url.search += (url.search ? '&' : '?') + 'cache-bust=' + now
-        }
-        return url.href
+    // 1. fixed http URL
+    // Just keep syncing with location.protocol
+    // fetch(httpURL) belongs to active mixed content.
+    // And fetch(httpRequest) is not supported yet.
+    url.protocol = self.location.protocol
+
+    // 2. add query for caching-busting.
+    // Github Pages served with Cache-Control: max-age=600
+    // max-age on mutable content is error-prone, with SW life of bugs can even extend.
+    // Until cache mode of Fetch API landed, we have to workaround cache-busting with query string.
+    // Cache-Control-Bug: https://bugs.chromium.org/p/chromium/issues/detail?id=453190
+    if (url.hostname === self.location.hostname) {
+        url.search += (url.search ? '&' : '?') + 'cache-bust=' + now
     }
+    return url.href
+}
 
-    /**
-     *  @Lifecycle Activate
-     *  New one activated when old isnt being used.
-     *
-     *  waitUntil(): activating ====> activated
-     */
-    self.addEventListener('activate', event => {
-      event.waitUntil(self.clients.claim())
-    })
+/**
+ *  @Lifecycle Activate
+ *  New one activated when old isnt being used.
+ *
+ *  waitUntil(): activating ====> activated
+ */
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim())
+})
 
-    /**
-     *  @Functional Fetch
-     *  All network requests are being intercepted here.
-     *
-     *  void respondWith(Promise<Response> r)
-     */
-    self.addEventListener('fetch', event => {
+/**
+ *  @Functional Fetch
+ *  All network requests are being intercepted here.
+ *
+ *  void respondWith(Promise<Response> r)
+ */
+self.addEventListener('fetch', event => {
     // Skip some of cross-origin requests, like those for Google Analytics.
     if (HOSTNAME_WHITELIST.indexOf(new URL(event.request.url).hostname) > -1) {
         // Stale-while-revalidate
@@ -53,7 +62,9 @@
         // Upgrade from Jake's to Surma's: https://gist.github.com/surma/eb441223daaedf880801ad80006389f1
         const cached = caches.match(event.request)
         const fixedUrl = getFixedUrl(event.request)
-        const fetched = fetch(fixedUrl, { cache: 'no-store' })
+        const fetched = fetch(fixedUrl, {
+            cache: 'no-store'
+        })
         const fetchedCopy = fetched.then(resp => resp.clone())
 
         // Call respondWith() with whatever we get first.
@@ -61,16 +72,20 @@
         // If there’s nothing in cache, wait for the fetch.
         // If neither yields a response, return offline pages.
         event.respondWith(
-        Promise.race([fetched.catch(_ => cached), cached])
+            Promise.race([fetched.catch(_ => cached), cached])
             .then(resp => resp || fetched)
-            .catch(_ => { /* eat any errors */ })
+            .catch(_ => {
+                /* eat any errors */
+            })
         )
 
         // Update the cache with the version we fetched (only for ok status)
         event.waitUntil(
-        Promise.all([fetchedCopy, caches.open("pwa-cache")])
+            Promise.all([fetchedCopy, caches.open("pwa-cache")])
             .then(([response, cache]) => response.ok && cache.put(event.request, response))
-            .catch(_ => { /* eat any errors */ })
+            .catch(_ => {
+                /* eat any errors */
+            })
         )
     }
-    })
+})
