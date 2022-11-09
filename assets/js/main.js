2ed61ebebdea46c58b9d5afb83fa07eb275b4673
Subhan Raj <shubhanraj2002@gmail.com>
2022-11-09 23:35:48 +0530

    Changed service worker

--- assets/js/main.js
+++ assets/js/main.js
@@ -267,16 +267,3 @@
 
 })()
 
-// Service Worker
-
-if ('serviceWorker' in navigator) {
-  // Register a service worker hosted at the root of the
-  // site using the default scope.
-  navigator.serviceWorker.register('./assets/js/service-worker.js').then((registration) => {
-    console.log('Service worker registration succeeded:', registration);
-  }, /*catch*/ (error) => {
-    console.error(`Service worker registration failed: ${error}`);
-  });
-} else {
-  console.error('Service workers are not supported.');
-}
