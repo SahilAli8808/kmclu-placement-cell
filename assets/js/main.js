8d65a88fa21ca08bbe3061c124779b4e74294c26
Subhan Raj <shubhanraj2002@gmail.com>
2022-11-09 23:30:07 +0530

    fixed service worker

--- assets/js/main.js
+++ assets/js/main.js
@@ -272,7 +272,7 @@
 if ('serviceWorker' in navigator) {
   // Register a service worker hosted at the root of the
   // site using the default scope.
-  navigator.serviceWorker.register('/assets/js/service-worker.js').then((registration) => {
+  navigator.serviceWorker.register('./assets/js/service-worker.js').then((registration) => {
     console.log('Service worker registration succeeded:', registration);
   }, /*catch*/ (error) => {
     console.error(`Service worker registration failed: ${error}`);
