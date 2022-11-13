5fa367f94868d39a96464b1eeb60cdb88643b041
Subhan Raj <shubhanraj2002@gmail.com>
2022-11-13 10:12:41 +0530

    Made service worker network first

--- service-worker.js
+++ service-worker.js
@@ -1,51 +1,43 @@
+const HOSTNAME_WHITELIST = [
+    self.location.hostname,
+    'fonts.gstatic.com',
+    'fonts.googleapis.com',
+    'cdn.jsdelivr.net'
+]
 
-    const HOSTNAME_WHITELIST = [
-        self.location.hostname,
-        'fonts.gstatic.com',
-        'fonts.googleapis.com',
-        'cdn.jsdelivr.net'
-    ]
+// The Util Function to hack URLs of intercepted requests
+const getFixedUrl = (req) => {
+    var now = Date.now()
+    var url = new URL(req.url)
 
-    // The Util Function to hack URLs of intercepted requests
-    const getFixedUrl = (req) => {
-        var now = Date.now()
-        var url = new URL(req.url)
 
-        
-        url.protocol = self.location.protocol
+    url.protocol = self.location.protocol
 
-        if (url.hostname === self.location.hostname) {
-            url.search += (url.search ? '&' : '?') + 'cache-bust=' + now
-        }
-        return url.href
+    if (url.hostname === self.location.hostname) {
+        url.search += (url.search ? '&' : '?') + 'cache-bust=' + now
+    }
+    return url.href
+}
+
+
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim())
+})
+
+
+//   fetch latest data from the internet and if the data is not available then use the cached data
+
+self.addEventListener('fetch', event => {
+    const req = event.request
+    const url = new URL(req.url)
+
+    if (req.method !== 'GET' || !HOSTNAME_WHITELIST.includes(url.hostname)) {
+        return
     }
 
-   
-    self.addEventListener('activate', event => {
-      event.waitUntil(self.clients.claim())
-    })
-
-    
-    self.addEventListener('fetch', event => {
-    // Skip some of cross-origin requests, like those for Google Analytics.
-    if (HOSTNAME_WHITELIST.indexOf(new URL(event.request.url).hostname) > -1) {
-       
-        const cached = caches.match(event.request)
-        const fixedUrl = getFixedUrl(event.request)
-        const fetched = fetch(fixedUrl, { cache: 'no-store' })
-        const fetchedCopy = fetched.then(resp => resp.clone())
-
-        event.respondWith(
-        Promise.race([fetched.catch(_ => cached), cached])
-            .then(resp => resp || fetched)
-            .catch(_ => { /* eat any errors */ })
-        )
-
-        // Update the cache with the version we fetched (only for ok status)
-        event.waitUntil(
-        Promise.all([fetchedCopy, caches.open("pwa-cache")])
-            .then(([response, cache]) => response.ok && cache.put(event.request, response))
-            .catch(_ => { /* eat any errors */ })
-        )
-    }
-    })
+    event.respondWith(
+        fetch(getFixedUrl(req), {
+            mode: 'cors'
+        }).catch(() => caches.match(req))
+    )
+})
